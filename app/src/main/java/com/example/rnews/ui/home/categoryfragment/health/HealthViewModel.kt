653e584dfca7f0bd0890e86f9ca1cb6a9c682eae
Rafi Dany Rasyad <Rafidanyr21@gmail.com>
2021-12-08 16:37:20 +0700

    Finished SearchFragment.kt and mvvm patern

---
+++ app/src/main/java/com/example/rnews/ui/home/categoryfragment/health/HealthViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.rnews.ui.home.categoryfragment.health
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.rnews.BuildConfig
+import com.example.rnews.api.ApiConfig
+import com.example.rnews.model.ArticleResponse
+import com.example.rnews.model.NewsResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class HealthViewModel: ViewModel() {
+
+    val listArticle = MutableLiveData<ArrayList<ArticleResponse>?>()
+
+    private val country = "id"
+    val category = "health"
+    private val apiKey = secret
+
+    fun setNews() {
+        ApiConfig.apiInstance
+            .getNewsCategory(country, category, apiKey)
+            .enqueue(object : Callback<NewsResponse> {
+                override fun onResponse(
+                    call: Call<NewsResponse>,
+                    response: Response<NewsResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        listArticle.postValue(response.body()?.modelArticle)
+                    }
+                }
+
+                override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                    Log.d("Failed to get data", t.message!!)
+                }
+            })
+    }
+
+    fun getNews(): MutableLiveData<ArrayList<ArticleResponse>?> {
+        return listArticle
+    }
+
+    companion object {
+        const val secret = BuildConfig.KEY
+    }
+}
